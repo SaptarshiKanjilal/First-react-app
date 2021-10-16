c2d34e34c08a4925160187371a1ee6c61a654f6f
Roshni <sparksroshni610@gmail.com>
2021-10-16 15:10:32 +0530

    first reacr app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
+import React,{ Fragment } from 'react';
+import './App.css';
+import Footer from './components/Footer';
+
+import Navbar from "./components/Navbar";
+import SidebarWrapper from './components/SidebarWrapper';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Fragment>
+      <Navbar/>
+   <SidebarWrapper/>
+    <Footer/>
+    </Fragment>
+    
+    )
 }
 
 export default App;
+
+//JSX
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React  from 'react'
+
+const Sidebar = ({count}) => {
+
+   
+    return (
+        <div className="eachSidebar">
+            This is  the sidebar. {count}
+        </div>
+    )
+}
+
+export default Sidebar
+
+
+
+//Hooks
